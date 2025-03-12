eb78b639b1994f390e2dcd2655f987f67a1b5ebe
Aldikitta <andi.kitta77@gmail.com>
2025-03-12 19:06:14 +0800

    feature: migrate to amper

--- core/data/movie/movie-data/src/commonMain/kotlin/com/aldikitta/core/data/movie/movie_data/api/MovieService.kt
+++
@@ -1,7 +0,0 @@
-package com.aldikitta.core.data.movie.movie_data.api
-
-internal interface MovieService {
-    fun setText() : String {
-        return "Hello"
-    }
-}
--- feature/dashboard/src/commonMain/kotlin/com/aldikitta/feature/dashboard/components/ItemMenu.kt
+++
@@ -1,9 +0,0 @@
-package com.aldikitta.feature.dashboard.components
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class ItemMenu(
-    val title: String,
-    val icon: ImageVector,
-    val navigateTo: () -> Unit
-)
