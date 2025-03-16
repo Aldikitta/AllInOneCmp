50dfa65a2db839d469daad82794ae2c701ecccc0
Aldikitta <andi.kitta77@gmail.com>
2025-03-16 12:17:14 +0800

    feat: amper 2

---
+++ base/navigation/src/Routes.kt
@@ -0,0 +1,26 @@
+import kotlinx.serialization.Serializable
+
+object Routes {
+    const val INITIAL = "initial"
+    const val MOVIE_DETAIL_SCREEN = "movie_detail_screen_routes"
+    const val MOVIE_HOME_SCREEN = "movie_home_screen_routes"
+    const val DASHBOARD = "dashboard_routes"
+}
+
+/*
+* Dashboard Navigation Graph
+* */
+@Serializable
+object DashboardNavGraph
+@Serializable
+object DashboardScreen
+
+/*
+* Movie Navigation Graph
+* */
+@Serializable
+object MovieNavGraph
+@Serializable
+object MovieHomeScreen
+@Serializable
+object MovieDetailScreen
